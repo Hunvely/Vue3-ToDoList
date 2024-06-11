e3f1bfd99c4a020a6a217e79dfc872cfd0eb4f51
Hunvely <jihoon9611@gmail.com>
2024-06-11 18:41:11 +0900

    Feat - Create a To-Do card and add Todo creation, deletion, and checking functions using Vue directive, checkbox, and style.

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,45 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit" class="d-flex">
-      <div class="flex-grow-1 mr-2">
-        <input
-          class="form-control"
-          type="text"
-          v-model="todo"
-          placeholder="Type new to-do"
-        />
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input
+            class="form-control"
+            type="text"
+            v-model="todo"
+            placeholder="Type new to-do"
+          />
+        </div>
+        <div>
+          <button class="btn btn-primary" type="submit">Add</button>
+        </div>
       </div>
-      <div>
-        <button class="btn btn-primary" type="submit">Add</button>
-      </div>
+      <div v-show="hasError" style="color: red">This field cannot be empty</div>
     </form>
-    {{ todos }}
+    <div v-if="!todos.length">
+      추가된 Todo가 없습니다.
+    </div>
+    <div v-for="(todo, index) in todos" v-bind:key="todo.id" class="card mt-2">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            v-model="todo.completed"
+          />
+          <label
+            class="form-check-label"
+            v-bind:style="todo.completed ? todoStyle : {}"
+          >
+            {{ todo.subject }}
+          </label>
+        </div>
+        <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">
+          Delete
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,17 +51,37 @@ export default {
   setup() {
     const todo = ref("");
     const todos = ref([]);
+    const hasError = ref(false);
+    const todoStyle = {
+      textDecoration: "line-through",
+      color: "gray",
+    };
+
     const onSubmit = () => {
-      todos.value.push({
-        id: Date.now(),
-        subject: todo.value,
-      });
+      if (todo.value === "") {
+        hasError.value = true;
+      } else {
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value,
+          completed: false,
+        });
+        hasError.value = false;
+        todo.value = "";
+      }
+    };
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
     };
 
     return {
       todo,
       todos,
       onSubmit,
+      hasError,
+      todoStyle,
+      deleteTodo
     };
   },
 };
@@ -44,7 +89,4 @@ export default {
 
 // CSS
 <style>
-.name {
-  color: blue;
-}
 </style>
