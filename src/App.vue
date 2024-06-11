594620780f8be7bbf20f55f231fa3fb46a20d6ea
Hunvely <jihoon9611@gmail.com>
2024-06-11 16:52:55 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+// HTML 
+<template>
+  <input type="text" v-model="name" />
+  <button class="btn btn-primary" @click="onSubmit">Click</button>
+</template>
+
+// JavaScript
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const name = ref("Hunvely");
+
+    const onSubmit = () => {
+      console.log(name.value);
+    };
+
+
+    return {
+      name,
+      onSubmit,
+    };
+  },
+};
+</script>
+
+// CSS
+<style>
+.name {
+  color: blue;
+}
+</style>
